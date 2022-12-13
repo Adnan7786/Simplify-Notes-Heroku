34a436763aecc11aaf2cf658016f7ecaf00d561d
Adnan Mallick <adnanmallick5178@gmail.com>
2022-12-13 18:03:21 +0530

    changed edit document method

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -21,7 +21,7 @@ router.route('/folder-tree').get(fetchFolderTree)
 router.route('/folder').post(createFolder).patch(renameFolder).delete(deleteFolder)
 router.route('/document').post(authorizeUser, createDocument).patch(renameDocument).delete(deleteDocument)
 router.route('/document/add').post(authorizeUser, addExisting)
-router.route('/document/edit').patch(setCurrEditingDoc)
+router.route('/document/edit').post(setCurrEditingDoc)
 
 
 module.exports = router
