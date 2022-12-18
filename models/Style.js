807a6e9c4fb3057ffd85ace8935e2314c8d3c275
Adnan Mallick <adnanmallick5178@gmail.com>
2022-12-18 19:53:38 +0530

    restored root folder creation only if not avail

--- models/Style.js
+++ models/Style.js
@@ -3,238 +3,239 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 
 const StyleSchema = mongoose.Schema({
-  heading: {
-    backgroundColor: {
-      red: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      blue: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      green: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      }
-    },
-    foregroundColor: {
-      red: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      blue: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      green: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      }
-    },
-    fontFamily: {
-      type: String,
-      enum: ['Arial', 'Open Sans', 'Poppins', 'Roboto'],
-      default: 'Arial'
-    },
-    fontSize: {
-      type: Number,
-      enum: [11, 15, 26],
-      default: 11
-    },
-    bold: {
-      type: Boolean,
-      default: false
-    },
-    italic: {
-      type: Boolean,
-      default: false
-    },
-    underline: {
-      type: Boolean,
-      default: false
-    }
-  },
-  subheading: {
-    backgroundColor: {
-      red: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      blue: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      green: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      }
-    },
-    foregroundColor: {
-      red: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      blue: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      green: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      }
-    },
-    fontFamily: {
-      type: String,
-      enum: ['Arial', 'Open Sans', 'Poppins', 'Roboto'],
-      default: 'Arial'
-    },
-    fontSize: {
-      type: Number,
-      enum: [11, 15, 26],
-      default: 11
-    },
-    bold: {
-      type: Boolean,
-      default: false
-    },
-    italic: {
-      type: Boolean,
-      default: false
-    },
-    underline: {
-      type: Boolean,
-      default: false
-    }
-  },
-  bullet: {
-    backgroundColor: {
-      red: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      blue: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      green: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      }
-    },
-    foregroundColor: {
-      red: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      blue: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      green: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      }
-    },
-    fontFamily: {
-      type: String,
-      enum: ['Arial', 'Open Sans', 'Poppins', 'Roboto'],
-      default: 'Arial'
-    },
-    fontSize: {
-      type: Number,
-      enum: [11, 15, 26],
-      default: 11
-    },
-    bold: {
-      type: Boolean,
-      default: false
-    },
-    italic: {
-      type: Boolean,
-      default: false
-    },
-    underline: {
-      type: Boolean,
-      default: false
-    }
-  },
-  paragraph: {
-    backgroundColor: {
-      red: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      blue: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      green: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      }
-    },
-    foregroundColor: {
-      red: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      blue: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      },
-      green: {
-        type: Number,
-        min: 0.0,
-        max: 1.0
-      }
-    },
-    fontFamily: {
-      type: String,
-      enum: ['Arial', 'Open Sans', 'Poppins', 'Roboto'],
-      default: 'Arial'
-    },
-    fontSize: {
-      type: Number,
-      enum: [11, 15, 26],
-      default: 11
-    },
-    bold: {
-      type: Boolean,
-      default: false
-    },
-    italic: {
-      type: Boolean,
-      default: false
-    },
-    underline: {
-      type: Boolean,
-      default: false
-    }
-  },
+  name: String
+  // heading: {
+  //   backgroundColor: {
+  //     red: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     blue: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     green: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     }
+  //   },
+  //   foregroundColor: {
+  //     red: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     blue: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     green: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     }
+  //   },
+  //   fontFamily: {
+  //     type: String,
+  //     enum: ['Arial', 'Open Sans', 'Poppins', 'Roboto'],
+  //     default: 'Arial'
+  //   },
+  //   fontSize: {
+  //     type: Number,
+  //     enum: [11, 15, 26],
+  //     default: 11
+  //   },
+  //   bold: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   italic: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   underline: {
+  //     type: Boolean,
+  //     default: false
+  //   }
+  // },
+  // subheading: {
+  //   backgroundColor: {
+  //     red: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     blue: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     green: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     }
+  //   },
+  //   foregroundColor: {
+  //     red: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     blue: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     green: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     }
+  //   },
+  //   fontFamily: {
+  //     type: String,
+  //     enum: ['Arial', 'Open Sans', 'Poppins', 'Roboto'],
+  //     default: 'Arial'
+  //   },
+  //   fontSize: {
+  //     type: Number,
+  //     enum: [11, 15, 26],
+  //     default: 11
+  //   },
+  //   bold: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   italic: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   underline: {
+  //     type: Boolean,
+  //     default: false
+  //   }
+  // },
+  // bullet: {
+  //   backgroundColor: {
+  //     red: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     blue: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     green: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     }
+  //   },
+  //   foregroundColor: {
+  //     red: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     blue: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     green: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     }
+  //   },
+  //   fontFamily: {
+  //     type: String,
+  //     enum: ['Arial', 'Open Sans', 'Poppins', 'Roboto'],
+  //     default: 'Arial'
+  //   },
+  //   fontSize: {
+  //     type: Number,
+  //     enum: [11, 15, 26],
+  //     default: 11
+  //   },
+  //   bold: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   italic: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   underline: {
+  //     type: Boolean,
+  //     default: false
+  //   }
+  // },
+  // paragraph: {
+  //   backgroundColor: {
+  //     red: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     blue: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     green: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     }
+  //   },
+  //   foregroundColor: {
+  //     red: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     blue: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     },
+  //     green: {
+  //       type: Number,
+  //       min: 0.0,
+  //       max: 1.0
+  //     }
+  //   },
+  //   fontFamily: {
+  //     type: String,
+  //     enum: ['Arial', 'Open Sans', 'Poppins', 'Roboto'],
+  //     default: 'Arial'
+  //   },
+  //   fontSize: {
+  //     type: Number,
+  //     enum: [11, 15, 26],
+  //     default: 11
+  //   },
+  //   bold: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   italic: {
+  //     type: Boolean,
+  //     default: false
+  //   },
+  //   underline: {
+  //     type: Boolean,
+  //     default: false
+  //   }
+  // },
 })
 
 module.exports = mongoose.model('Style', StyleSchema)
