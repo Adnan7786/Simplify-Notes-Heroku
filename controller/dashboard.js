e52cc2c83531ba82bab10d7508563836e18d154f
Adnan Mallick <adnanmallick5178@gmail.com>
2022-12-18 19:36:22 +0530

    Revert "changed edit document method"
    This reverts commit 4f78b171b53f9f39837cecb7ceef8eac5a413675.

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -332,7 +332,8 @@ const setCurrEditingDoc = async (req, res) => {
     user: userId
   })
 
-  if (currentDoc && currentDoc._id == documentId) {
+  console.log(currentDoc?._id, documentId);
+  if (currentDoc && currentDoc._id === documentId) {
     throw new BadRequestError('Already editing the specified document');
   }
 
