040f2efd6fa2a4b21a3ad2f8f385badd3642c506
Adnan Mallick <adnanmallick5178@gmail.com>
2022-12-31 21:28:00 +0530

    Limit update

--- api/index.js
+++ api/index.js
@@ -58,6 +58,8 @@ app.use(cors())
 app.use(xss())
 app.use(mongoSanitize())
 app.use(morgan('tiny')) // log format ':method :url :status :res[content-length] - :response-time ms'
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 // parse form data
 app.use(express.urlencoded({ extended: false })) // parse form data
 app.use(express.json()) //parse json
